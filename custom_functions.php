b6b0871948bc74d0cc259cd8f01daff315440d58
Mikaela Ehly <mikaela.ehly@tunatraffic.com>
2015-06-11 09:42:12 -0700

    Deleted custom_functions.php

--- custom_functions.php
+++
@@ -1,5 +0,0 @@
-function sButton($atts, $content = null) {
-   extract(shortcode_atts(array('link' => '#'), $atts));
-   return '<a class="button" href="'.$link.'"><span>' . do_shortcode($content) . '</span></a>';
-}
-add_shortcode('button', 'sButton');
